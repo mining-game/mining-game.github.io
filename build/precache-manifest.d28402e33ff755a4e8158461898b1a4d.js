28290b561527cd81d0d73010f6b606c5d7f3faf8
ivankonst174 <ivankonst174@example.com>
2020-04-08 22:45:14 +0500

    test174

---
+++ build/precache-manifest.d28402e33ff755a4e8158461898b1a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d886eecc1de4fc4261ac72bfa5d5bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e48e59e232534a207eb",
+    "url": "/static/css/main.92f12d1c.chunk.css"
+  },
+  {
+    "revision": "9109c08cd41f81aca776",
+    "url": "/static/js/2.1838e4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1838e4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e48e59e232534a207eb",
+    "url": "/static/js/main.78d4b433.chunk.js"
+  },
+  {
+    "revision": "e8134d115cba7592aff2",
+    "url": "/static/js/runtime-main.63854b69.js"
+  }
+]);
